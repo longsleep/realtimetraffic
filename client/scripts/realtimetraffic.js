24033fc3ee298cc0f4133e9b19fd95eecc39d8a8
longsleep <longsleep@adde93d6-dd1c-0410-b80c-dff0d6e00c6c>
2012-12-12 12:07:56 +0000

    added ssl support

---
+++ client/scripts/realtimetraffic.js
@@ -0,0 +1,209 @@
+(function() {
+
+    var RealtimeTraffic = function() {
+    
+        this.tv = 500;
+        this.last_data = null;
+        this.connection = null;
+        this.initialize();
+        
+    }
+    
+    RealtimeTraffic.prototype.initialize = function() {
+
+        var graph = this.graph = new Rickshaw.Graph( {
+            element: document.getElementById("chart"),
+            height: 300,
+            width: 690,
+            renderer: 'line',
+            interpolation: 'linear',
+            series: new Rickshaw.Series.FixedDuration([ { name: 'rx_tx_abs_diff', color: "palegreen" }, { name: 'rx_kbits', color: "mediumslateblue" }, { name: 'tx_kbits', color: "lightcoral" }], undefined, {
+                timeInterval: this.tv,
+                maxDataPoints: 200,
+                timeBase: new Date().getTime() / 1000
+            }) 
+        } );
+
+        var xAxis = new Rickshaw.Graph.Axis.Time({
+            graph: graph
+        });
+
+        var yAxis = new Rickshaw.Graph.Axis.Y({
+            graph: graph,
+            tickFormat: Rickshaw.Fixtures.Number.formatKMBT
+        });
+
+        var legend = new Rickshaw.Graph.Legend({
+            graph: graph,
+            element: document.getElementById('legend')
+        });
+
+        var hoverDetail = new Rickshaw.Graph.HoverDetail({
+            graph: graph
+        });
+
+        var smoother = new Rickshaw.Graph.Smoother({
+            graph: graph,
+            element: document.getElementById('smoother')
+        });
+
+        var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
+            graph: graph,
+            legend: legend
+        });
+
+        this.graph.render();
+
+    };
+
+    RealtimeTraffic.prototype.disconnect = function() {
+    
+        if (this.connection) {
+            this.connection.close();
+            this.connection = this.last_data = null;
+            this.needsReset = true;
+        }
+    
+    };
+
+    RealtimeTraffic.prototype.connect = function(url, interf) {
+
+        var ws_url = url+'?if='+interf;
+
+        this.disconnect();
+        
+        if (this.needsReset) {
+            $("#chart").empty();
+            $("#legend").empty();
+            this.initialize();
+        }
+        
+        var connection;
+        var that = this;
+
+        console.info("Connecting to " + ws_url);
+        try {
+            connection = this.connection = new WebSocket(ws_url);
+        } catch(e) {
+            console.error("Failed to create websocket connection to " + ws_url, e);
+            alert("Failed to connect. See console for details.");
+            return;
+        }
+
+        connection.onopen = function () {
+            // do nothing
+            console.info("Connection established.");
+            $(that).triggerHandler("open");
+        };
+
+        connection.onclose = function() {
+            console.info("Connection closed.");
+            $(that).triggerHandler("close");
+        };
+
+        // Log errors
+        connection.onerror = function (error) {
+            console.error('WebSocket error ' + error);
+            alert("Connection error. See console for details.");
+        };
+
+        connection.onmessage = function(e) {
+            var data = JSON.parse(e.data);
+            var interface_data = data[interf];
+            if (typeof(interface_data) !== "undefined") {
+                if (that.last_data !== null) {
+                    var d = {
+                        rx_kbits: ((interface_data.rx_bytes - that.last_data.rx_bytes) * 8 / 1024)*2,
+                        tx_kbits: ((interface_data.tx_bytes - that.last_data.tx_bytes) * 8 / 1024)*2
+                    };
+                    d.rx_tx_abs_diff = Math.abs(d.rx_kbits - d.tx_kbits);
+                    setTimeout(function() {
+                        that.graph.series.addData(d);
+                        that.graph.render();
+                    }, 0);
+                }
+                that.last_data = interface_data;
+            }
+        };
+        
+    };
+
+$(document).ready(function() {
+
+    var control = $("#control");
+    var input_url = $("#input_url");
+    var input_interf = $("#input_interf");
+    var button_start = $("button.start", control);
+    var button_stop = $("button.stop", control);
+
+    var url;
+    var interf;
+
+    // Read defaults from params or localStorage
+    var params = jQuery.deparam.querystring();
+    if (params.url || params.interf) {
+        url = $.trim(params.url);
+        interf = $.trim(params.interf);
+    } else if (Modernizr.localstorage) {
+        url = localStorage.getItem("org.longsleep.realtimetraffic.defaults.url");
+        interf = localStorage.getItem("org.longsleep.realtimetraffic.defaults.interf");
+    }
+    
+    if (url) {
+        input_url.val(url);
+    }
+    if (interf) {
+        input_interf.val(interf);
+    }
+
+    if (!Modernizr.websockets || !Modernizr.svg) {
+        $("button").prop("disabled", true);
+        alert("Your browser seems to lack required features to run this application. Launch aborted.");
+        return;
+    }
+
+    var realtimetraffic = new RealtimeTraffic();
+    $(realtimetraffic).on("open", function() {
+        control.addClass("connected");
+    });
+    $(realtimetraffic).on("close", function() {
+        control.removeClass("connected");
+    });
+    
+    button_start.on("click", function() {
+    
+        url = $.trim(input_url.val());
+        interf = $.trim(input_interf.val());
+        
+        if (!url || !interf) {
+            return;
+        }
+        
+        realtimetraffic.connect(url, interf);
+        
+        if (Modernizr.localstorage) {
+            try {
+                localStorage.setItem("org.longsleep.realtimetraffic.defaults.url", url);
+                localStorage.setItem("org.longsleep.realtimetraffic.defaults.interf", interf);
+            } catch(e) {
+                console.warn("Failed to store settings into localStorage with error " + e);
+            }
+        }
+        
+    });
+    
+    button_stop.on("click", function() {
+    
+        realtimetraffic.disconnect();
+    
+    });
+    
+    if (params.autostart === "1") {
+        button_start.click();
+    }
+
+});
+
+}());
+
+
---
+++ trafficserver/trafficserver.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python
+
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+
+import os
+import sys
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Globals
+dataGrabbers = {}
+dataGrabbed = {}
+CLIENT_ROOT = os.path.abspath(os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "client")))
+
+
+class Grabber(object):
+
+    keys = [
+    "collisions",
+    "multicast",
+    "rx_bytes",
+    "rx_compressed",
+    "rx_crc_errors",
+    "rx_dropped",
+    "rx_errors",
+    "rx_fifo_errors",
+    "rx_frame_errors",
+    "rx_length_errors",
+    "rx_missed_errors",
+    "rx_over_errors",
+    "rx_packets",
+    "tx_aborted_errors",
+    "tx_bytes",
+    "tx_carrier_errors",
+    "tx_compressed",
+    "tx_dropped",
+    "tx_errors",
+    "tx_fifo_errors",
+    "tx_heartbeat_errors",
+    "tx_packets",
+    "tx_window_errors"
+    ]
+
+    def __init__(self, interface):
+        self.interface = interface
+        self.base = "/sys/class/net/%s/statistics" % interface
+
+    def grab(self):
+
+        data = {}
+        for k in self.keys:
+            data[k] = self.read(k)
+
+        dataGrabbed[self.interface]=data
+
+    def read(self, key):
+        data = 0
+        fn = os.path.join(self.base, key)
+        if os.path.exists(fn):
+            fp = file(fn, "rb")
+            try:
+                data = fp.read()
+            finally:
+                fp.close()
+            data = int(data.strip())
+        return data
+
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+
+    dataSender = None
+    dataInterface = None
+
+    def open(self):
+
+        interface = self.dataInterface = self.get_argument("if", "eth0").lower()
+
+        grabber = dataGrabbers.get(interface, None)
+        if grabber is None:
+            # create new grabber
+            print >>sys.stdout, "Starting grabber for %s." % interface
+            callback = Grabber(interface)
+            grabber = tornado.ioloop.PeriodicCallback(callback.grab, 240)
+            grabber.start()
+            dataGrabbers[interface]=[1, grabber]
+        else:
+            dataGrabbers[interface][0] = grabber[0]+1
+
+        sender = self.dataSender = tornado.ioloop.PeriodicCallback(self.doSendData, 500)
+        sender.start()
+
+    def on_message(self, message):
+        pass
+
+    def on_close(self):
+
+        self.dataSender.stop()
+        self.dataSender = None
+
+        grabber = dataGrabbers.get(self.dataInterface, None)
+        if grabber is not None:
+            dataGrabbers[self.dataInterface][0] = grabber[0]-1
+            if grabber[0] <= 0:
+                print >>sys.stdout, "Stopping grabber for %s." % self.dataInterface
+                grabber[1].stop()
+                del dataGrabbers[self.dataInterface]
+
+    def doSendData(self):
+
+        data = {
+            self.dataInterface: dataGrabbed.get(self.dataInterface, {})
+        }
+        self.write_message(json.dumps(data))
+
+
+class ClientHandler(tornado.web.RequestHandler):
+
+    def get(self, filename):
+
+        if not filename or filename == "index.html":
+            filename = "realtimetraffic.html"
+
+        fn = os.path.abspath(os.path.join(CLIENT_ROOT, os.path.normpath(filename)))
+        if not fn.startswith(CLIENT_ROOT):
+            raise tornado.web.HTTPError(404)
+        if not os.path.isfile(fn):
+            raise tornado.web.HTTPError(404)
+        fp = file(fn, "rb")
+        try:
+            self.write(fp.read())
+        finally:
+            fp.close()
+
+
+def main(listen="127.0.0.1:8088"):
+
+    from optparse import OptionParser
+    
+    parser = OptionParser()
+    parser.add_option("-l", "--listen", dest="listen", help="listen address (default: [%s])" % listen, default=listen)
+    parser.add_option("--ssl_keyfile", dest="ssl_keyfile", help="SSL key file", metavar="FILE")
+    parser.add_option("--ssl_certfile", dest="ssl_certfile", help="SSL certificate file", metavar="FILE")
+
+    (options, args) = parser.parse_args()
+
+    if ":" in options.listen:
+        address, port = options.listen.split(":", 1)
+        port = int(port)
+        listen = options.listen
+    else:
+        address = options.listen
+        port = 8088
+        listen = "%s:%s" % (address, port)
+
+    application = tornado.web.Application([
+        (r'^/ws$', WSHandler),
+        (r'^/css/(.*)$', tornado.web.StaticFileHandler, {'path': os.path.join(CLIENT_ROOT, 'css')}),
+        (r'^/scripts/(.*)$', tornado.web.StaticFileHandler, {'path': os.path.join(CLIENT_ROOT, 'scripts')}),
+        (r'^/img/(.*)$', tornado.web.StaticFileHandler, {'path': os.path.join(CLIENT_ROOT, 'img')}),
+        (r'^/(.*)$', ClientHandler)
+    ], debug=False, static_path=CLIENT_ROOT)
+
+    params = {}
+    ssl = False
+    if options.ssl_keyfile and options.ssl_certfile:
+        if not os.path.isfile(options.ssl_keyfile):
+            print >>sys.stderr, "SSL key file not found: %s" % options.ssl_keyfile
+            return 1
+        if not os.path.isfile(options.ssl_certfile):
+            print >>sys.stderr, "SSL certificate file not found: %s" % options.ssl_certfile
+            return 1
+        params["ssl_options"] = {
+            "keyfile": options.ssl_keyfile,
+            "certfile": options.ssl_certfile
+        }
+        ssl = True
+
+    http_server = tornado.httpserver.HTTPServer(application, **params)
+    http_server.listen(port=port, address=address)
+    print >>sys.stdout, "Server running on %s (ssl:%r) ..." % (listen, ssl)
+    try:
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        pass
+    print >>sys.stdout, "Server stopped."
+    return 0
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
+    
+
